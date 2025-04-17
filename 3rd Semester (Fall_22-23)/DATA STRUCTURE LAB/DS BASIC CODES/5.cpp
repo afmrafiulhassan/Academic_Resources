27c72b2a89ac6bf5196b864e5e6cdd50b11d0348
AFM Rafiul Hassan <114606137+afmrafiulhassan@users.noreply.github.com>
2025-04-17 23:57:00 +0600

    Add files via upload

---
+++ 3rd Semester (Fall_22-23)/DATA STRUCTURE LAB/DS BASIC CODES/5.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int N;
+    int P;
+    int fac=1;
+    cin>>N;
+    for(int i = 2; i <= N; i++){
+        P = 1;
+        for(int j = 2; j <= i/2; j++){
+             if(i % j == 0 && N!= 0 && N!=1){
+                 P = 0;
+             }
+        }
+    }
+    if(P==1){
+        for(int i=1; i<=N;i++){
+            fac *=i;
+        }
+        cout<<fac;
+    }
+    else{
+        cout<<"Error! Not a prime number.";
+    }
+
+
+}
