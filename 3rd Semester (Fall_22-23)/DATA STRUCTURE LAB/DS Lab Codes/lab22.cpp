890482948a6876b4e80bb9027e20f8c39991e2ce
AFM Rafiul Hassan <114606137+afmrafiulhassan@users.noreply.github.com>
2025-04-19 21:47:31 +0600

    Add files via upload

---
+++ 3rd Semester (Fall_22-23)/DATA STRUCTURE LAB/DS Lab Codes/lab22.cpp
@@ -0,0 +1,50 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int arr1[6] = {1,4,6,3,6,9};
+    int arr2[6] = {5,3,7,1,2,6};
+    int arr3[10];
+    int n=0;
+
+    cout<<endl;
+    cout<<"Array 1 elements "<<endl;
+    for(int i=0; i<6; i++)
+    {
+        cout<<arr1[i]<<"\t";
+    }
+
+    cout<<endl;
+    cout<<"Array 2 elements "<<endl;
+    for(int i=0; i<6; i++)
+    {
+        cout<<arr2[i]<<"\t";
+    }
+
+    for(int i = 0 ; i < 6; i++)
+    {
+        for (int j = 0 ; j < 6; j++)
+        {
+            if(arr1[i] == arr2[j])
+            {
+                arr3[n] = arr2[j];
+                n++;
+            }
+        }
+    }
+
+    if(n == 0)
+    {
+        cout<<"\n\nNo Common Element"<<endl<<endl;
+    }
+    else
+    {
+        cout<<"\n\nThe common elements: "<<endl;
+        for (int i = 0; i<n; i++)
+        {
+                cout << arr3[i] << " ";
+        }
+        cout<<endl<<endl;
+    }
+}
