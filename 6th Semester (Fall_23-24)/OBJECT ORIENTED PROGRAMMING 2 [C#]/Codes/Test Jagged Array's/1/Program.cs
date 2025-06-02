9023d6797366230cdcf7d2e2011a269ea9c56196
AFM Rafiul Hassan <114606137+afmrafiulhassan@users.noreply.github.com>
2025-06-02 20:19:46 +0600

    Add files via upload

---
+++ 6th Semester (Fall_23-24)/OBJECT ORIENTED PROGRAMMING 2 [C#]/Codes/Test Jagged Array's/1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 1
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            // ** Array in c# ***
+            //Let's declare and intialize a single dimensional array
+            int[] a = new int[5] { 1, 2, 3, 4, 5 };
+            int[] arr = { 1, 3, 5, 7, 9 };
+            // we can also wrtie like int[] a = new int[] {1, 2, 3, 4, 5};
+            // we can even also write like int[] = {1, 2, 3, 4, 5};
+            //array_name.Length will return the size of the array                    
+
+            // printing the elements of the first array
+            for (int i = 0; i < a.Length; i++)
+            {
+                Console.WriteLine(a[i]);
+            }
+
+            // printing the elements of the second array 
+            for (int i = 0; i < a.Length; i++)
+            {
+                Console.WriteLine(arr[i]);
+            }
+        }
+    }
+}
---
+++ 6th Semester (Fall_23-24)/OBJECT ORIENTED PROGRAMMING 2 [C#]/Codes/Test Jagged Array's/Test Jagged Array's/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace jagged_array
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[][] jagged_array = new int[3][];
+            jagged_array[0] = new int[] { 1, 2, 3, 3 };
+            jagged_array[1] = new int[] { 4, 5, 6, 7 };
+            jagged_array[2] = new int[] { 4, 5, 3 };
+            for (int i = 0; i < jagged_array.Length; i++)
+            {
+                //Print the row number
+                Console.WriteLine("Row {0}", i);
+                for (int j = 0; j < jagged_array[i].Length; j++)
+                {
+                    Console.WriteLine("{0}", jagged_array[i][j]);
+                }
+                Console.WriteLine();
+            }
+            Console.ReadLine();
+        }
+    }
+}
